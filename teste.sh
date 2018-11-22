07a0c0d6df0a4cf3635e6c35f8b9c42ca765a804
David Alves <david27alves@hotmail.com>
2018-11-22 10:05:26 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,9 +1,64 @@
 #!/usr/bin/env bash
 
-echo "passou direto"
+latest_version()
+{
+  echo "1.0.0"
+}
 
-read IP
+update_repository()
+{
+  sudo apt-get -y update
+  sudo apt-get install vim -y
+}
 
-echo $IP
+resolution()
+{
+  sudo xrandr -s 800x600
+  sudo xset -dpms
+  sudo xset s off
+}
 
-echo "nao leu"
+create_dir()
+{
+  if [ ! -d "/vr/" ]; then
+    mkdir /vr/
+  fi
+
+  if [ ! -d "/pdv/exec" ]; then
+    mkdir /pdv/exec
+  fi
+
+  if [ ! -d "/pdv_vr" ]; then
+    mkdir /pdv_vr/
+  fi
+
+  sudo chmod -R 2777 /vr/
+  sudo chmod -R 2777 /pdv/
+  sudo chmod -R 2777 /home/$USER/
+}
+
+mount_server()
+{
+  IP=$(whiptail --inputbox "Digite o IP do Servidor" 8 78 --title "Instalação VRPdv Linux" 3>&1 1>&2 2>&3)
+  sudo chmod +777 /etc/fstab
+  echo //$IP/vr /pdv_vr/ cifs username=pdv,password=pdv 0 0 >> /etc/fstab
+  sudo mount //$IP/vr /pdv_vr/ -o username=pdv,password=pdv
+}
+
+copy_files()
+{
+  echo "Copying libs..."
+  sudo cp -r /pdv_vr/exec/lib/ /pdv/exec/
+  echo "Copying database..."
+  sudo cp -r /pdv_vr/pdv/database/VR.FDB /pdv/database/
+  sudo chmod g+w /pdv/database/VR.FDB
+}
+
+
+
+#main script
+
+latest_version
+mount_server
+create_dir
+copy_files
